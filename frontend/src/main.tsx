b5bd061704b910b6182837d058b1ac38a0854af8
ufxeren <ufxeren@gmail.com>
2025-12-30 15:00:58 +0500

    add some more file for deploying on github pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
+import { HashRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: './',
+    base: '/UFX-Portfolio/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
